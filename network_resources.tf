f5527bc689e5e47ee82f575d365d91a1b36b58dc
Bailey Young <yamabay@yamabay>
2022-08-25 17:47:09 +0900

    Fix backend

--- network_resources.tf
+++ network_resources.tf
@@ -1,12 +1,23 @@
 terraform {
-provider "aws" {
-  source  = "hashicorp/aws"
-  region  = "us-west-2"
-  alias   = "us-west"
- }
-provider "aws" {
-  source  = "hashicorp/aws"
-  region = "ap-northeast-1"
-  alias  = "ap-northeast-1"
- }
+
+  backend "local" {}
+
 }
+provider "aws" {
+
+  source  = "hashicorp/aws"
+  
+  region  = "us-west-2"
+  
+  alias   = "us-west"
+
+ }
+provider "aws" {
+
+  source  = "hashicorp/aws"
+
+  region = "ap-northeast-1"
+
+  alias  = "tokyo"
+
+ }
