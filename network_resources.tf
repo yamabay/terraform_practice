69e4d9c5019ef7cc39af1f19d7b120df8a1fbc7f
Bailey Young <yamabay@yamabay>
2022-08-28 17:36:53 +0900

    Fix the syntax issues

--- network_resources.tf
+++ network_resources.tf
@@ -4,40 +4,30 @@ terraform {
 
 }
 provider "aws" {
- 
+
   region  = "us-west-2"
-  alias   = "us-west-2"
+  profile = "terraform_user"
 
- }
-provider "aws" {
-
-  region = "us-east-1"
-  alias  = "us-east-1"
-
- }
+}
 
 module "vpc" {
-
-  source = "terraform-aws-modules/vpc/aws"
-  cidr   = "192.168.1.0/16"
-  azs    =  "us-west-2a"
+  source         = "terraform-aws-modules/vpc/aws"
+  cidr           = "192.168.0.0/16"
+  azs            = ["us-west-2a"]
   public_subnets = ["192.168.1.0/24"]
-
-  providers = {
-    aws = aws.us-west-2 
-   }
-  output "vpc_id" {
-    value = aws
+  tags = {
+    Name = "Uchi_VPC"
   }
 }
-#module "vpc" {
-#  
-#  source = "terraform-aws-modules/vpc/aws"
-#  cidr   = "10.0.0.0/16"
-#  azs    = "us-east-1a"
-#  public_subnets = ["10.0.2.0/24", "10.0.1.0/24"]
-#
-#  providers = {
-#    aws = aws.us-east-1
-#  }
-#}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "test_subnet_1" {
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "192.168.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = "test_subnet_1"
+  }
+}
