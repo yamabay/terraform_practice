92fe82380c9c511216afbe4a89c678d8f44ecbbc
Bailey Young <yamabay@yamabay>
2022-08-28 10:05:59 +0900

    Initial commit on test infrastructure build

--- network_resources.tf
+++ network_resources.tf
@@ -4,26 +4,40 @@ terraform {
 
 }
 provider "aws" {
-  
+ 
   region  = "us-west-2"
-  
-  alias   = "us-west"
+  alias   = "us-west-2"
 
  }
 provider "aws" {
 
-  region = "ap-northeast-1"
-
-  alias  = "tokyo"
+  region = "us-east-1"
+  alias  = "us-east-1"
 
  }
+
 module "vpc" {
 
   source = "terraform-aws-modules/vpc/aws"
-
-  cidr   = "10.200.0.0/16"
+  cidr   = "192.168.1.0/16"
+  azs    =  "us-west-2a"
+  public_subnets = ["192.168.1.0/24"]
 
   providers = {
-    name = us-west 
+    aws = aws.us-west-2 
    }
+  output "vpc_id" {
+    value = aws
+  }
 }
+#module "vpc" {
+#  
+#  source = "terraform-aws-modules/vpc/aws"
+#  cidr   = "10.0.0.0/16"
+#  azs    = "us-east-1a"
+#  public_subnets = ["10.0.2.0/24", "10.0.1.0/24"]
+#
+#  providers = {
+#    aws = aws.us-east-1
+#  }
+#}
