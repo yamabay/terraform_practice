0f3d5f560e6898d1a023345c0d6f3926ef10e185
Bailey Young <yamabay@yamabay>
2022-08-31 15:48:07 +0900

    Fix unneeded comments

--- ec2_instance_creation.tf
+++ ec2_instance_creation.tf
@@ -4,8 +4,6 @@ resource "aws_instance" "new_ec2" {
   ami               = "ami-05fa00d4c63e32376"
   instance_type   = "t2.micro"
   availability_zone = "us-east-1a"
-  #host_recovery     = "on"
-  #auto_placement    = "on"
   provider          = aws.us-east-1
   tags = {
     Name = "Test EC2 Instance"
